b71a0a13074a41d95d2b5934eff66aed3682a267
Okan Binli <okanb3@gmail.com>
2015-08-15 10:18:38 +0300

    Selectbox ve user show kısmı yapıldı.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,6 +38,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:title, :description, :price)
+    params.require(:product).permit(:title, :description, :price, :user_id)
   end
 end
